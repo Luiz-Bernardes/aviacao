a2ab9dc206dafdd986bc4096c47bf7403ff91df1
Luiz-Bernardes <soumaisluiz@hotmail.com>
2012-10-21 12:28:37 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aviacao::Application.config.secret_token = '48149cc90c3dec23b70af7d0eacec14c32a46264cfbe8751bc252a695c8e9f43dccedf8d6bb204a7a7ed21d6cfd00c0dbcd48d920800b4ecd0b1a85da5bf11e7'
